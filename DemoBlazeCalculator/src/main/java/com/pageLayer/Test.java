fea7534147d3818d633ab0cc10624034b3864e2f
nayanyerkade <nayan.yerkade@gmail.com>
2022-11-23 16:03:36 +0530

    add

---
+++ DemoBlazeCalculator/src/main/java/com/pageLayer/Test.java
@@ -0,0 +1,16 @@
+package com.pageLayer;
+
+public class Test {
+ public static void main(String[] args) {
+	Test t= new Test();
+	t.add();
+}
+ 
+ public void add()
+ {
+	 int a=10;
+	 int b=20;
+	 
+	 System.out.println("Add==="+(a+b));
+ }
+}
